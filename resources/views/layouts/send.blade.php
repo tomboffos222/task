252af65309eea48a93c01b205bfc1fb69210dfb2
Asyl Tas <asyltas@MacBook-Pro-Asyl.local>
2020-05-12 04:04:39 +0600

    new

--- resources/views/layouts/send.blade.php
+++ resources/views/layouts/send.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta property="og:locale" content="en_US" />
     <title>@yield('title')</title>
     <meta property="og:url" content="{{ Request::url() }}">
     <meta property="og:type" content="website" >
