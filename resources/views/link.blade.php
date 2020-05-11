422108083d55db7438602c1d6a8e69c699a4d1f2
Asyl Tas <asyltas@MacBook-Pro-Asyl.local>
2020-05-12 01:53:52 +0600

    new

--- resources/views/link.blade.php
+++ resources/views/link.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{$title}}</title>
-    <meta property="og:url" content="http://laravelprojectone.herokuapp.com/">
+    <meta property="og:url" content="{{$url}}">
     <meta property="og:type" content="website" />
     <meta property="og:title" content="{{$title}}" />
     <meta property="og:description" content="{{$description}}" />
