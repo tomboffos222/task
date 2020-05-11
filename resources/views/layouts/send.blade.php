1f6f21d4b8d81d76f88257d42300201a6a028f5e
Asyl Tas <asyltas@MacBook-Pro-Asyl.local>
2020-05-12 04:10:45 +0600

    new

--- resources/views/layouts/send.blade.php
+++ resources/views/layouts/send.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta property="og:locale" content="en_US" />
-    <title>@yield('title')</title>
+    <title>Laravel</title>
     <meta property="og:url" content="{{ Request::url() }}">
     <meta property="og:type" content="website" >
 
--- resources/views/link.blade.php
+++ resources/views/link.blade.php
@@ -1,4 +1,14 @@
+
 @extends('layouts.send')
+@php
+$links = session()->get('links');
+$link = $links[$id];
+$title = $link['title'];
+$description = $link['description'];
+$image = route('Links').$link['image'];
+
+
+@endphp
 @section('title'){!! $title !!}@endsection
 @section('description'){!! $description !!}@endsection
 @section('image'){!! $image !!}@endsection
