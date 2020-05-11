cc01fbfde3dda93a5d5fc1e6980406a6d6d12a32
Asyl Tas <asyltas@MacBook-Pro-Asyl.local>
2020-05-12 03:07:08 +0600

    new

--- resources/views/link.blade.php
+++ resources/views/link.blade.php
@@ -5,14 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{$data->link['title']}}</title>
+    <title>{{$link['title']}}</title>
     <meta property="og:type" content="website" >
 
-    <meta property="og:title" content="{{$data->link['title']}}" >
-    <meta property="og:image" content="{{$data->image}}" >
+    <meta property="og:title" content="{{$link['title']}}" >
+    <meta property="og:image" content="{{$image}}" >
 
 
-    <meta property="og:description" content="{{$data->link['title']}}" >
+    <meta property="og:description" content="{{$link['title']}}" >
     <meta property="og:url" content="{{ Request::url() }}">
 
 </head>
