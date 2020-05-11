a8d10c2c00a1538e113e6412a3a071ae2cbd3dc4
Asyl Tas <asyltas@MacBook-Pro-Asyl.local>
2020-05-12 03:33:45 +0600

    new

--- resources/views/link.blade.php
+++ resources/views/link.blade.php
@@ -1,13 +1,5 @@
 @extends('layouts.send')
-@section('title')
-    {{$link['title']}}
-@endsection
-@section('image')
-    {{$image}}
-@endsection
-@section('description')
-    {{$link['description']}}
-@endsection
-@section('link')
-    {{$link['link']}}
-@endsection
+@section('title'){{$link['title']}}@endsection
+@section('image'){{$image}}@endsection
+@section('description'){{$link['description']}}@endsection
+@section('link'){{$link['link']}}@endsection
